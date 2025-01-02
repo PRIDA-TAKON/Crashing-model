34ef89e4d3324d7695b8107d5cbda9cbb3faf2da
PRIDA-TAKON <takonaom@gmail.com>
2025-01-03 00:28:02 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,8 @@ apt-get update
 # ติดตั้ง Graphviz และ dependencies
 apt-get install -y graphviz libgraphviz-dev pkg-config
 
-
-# ติดตั้ง pygraphviz พร้อมระบุเส้นทาง
-pip install pygraphviz --global-option=build_ext --global-option="-I/usr/include/graphviz" --global-option="-L/usr/lib/graphviz"
+# ติดตั้ง PyGraphviz พร้อมกำหนดเส้นทาง
+pip install pygraphviz --global-option=build_ext --global-option="-I/usr/include/graphviz" --global-option="-L/usr/lib"
 
 # ติดตั้ง Python libraries จาก requirements.txt
 pip install -r requirements.txt
