eb8482d58210fd34187dd73d00f8fc9abb3960b9
PRIDA-TAKON <takonaom@gmail.com>
2025-01-03 13:08:20 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # อัปเดต apt-get และติดตั้ง Graphviz
-apt-get update && apt-get install -y graphviz graphviz-dev
+apt-get update && apt-get install -y graphviz==0.20.3 graphviz-dev
 
 # ติดตั้ง pygraphviz จาก GitHub
-pip install pygraphviz
+pip install pygraphviz==1.7
 
 # ติดตั้ง dependencies อื่นๆ จาก requirements.txt
 pip install -r requirements.txt
